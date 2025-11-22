fbf0b4d3a07e459392a1d9588a9a97a6e18e1087
vaishnavi2628 <vaishnavimishra840@gmail.com>
2025-11-22 15:07:53 +0530

    temperature fixed

--- script.js
+++ script.js
@@ -91,7 +91,9 @@ function renderWeatherInfo(weatherInfo){
   countryIcon.src=`https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
   desc.innerText=weatherInfo?.weather?.[0]?.description;
   weatherIcon.src=`http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-  temp.innerText= `${weatherInfo?.main?.temp} c°`;
+  const kelvinTemp = weatherInfo?.main?.temp;
+  const celsiusTemp = kelvinTemp - 273.15;
+  temp.innerText = `${celsiusTemp.toFixed(1)}℃`; 
   windSpeed.innerText=`${weatherInfo?.wind?.speed} m/s`;
   humidity.innerText=`${weatherInfo?.main?.humidity} %`;
   cloudiness.innerText=`${weatherInfo?.clouds?.all} %`;
